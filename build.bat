4d8e75edd97cc9e0657e99da3dbf29d870aab7c7
arshavir mirzakhani <arshavir2468@gmail.com>
2025-05-17 17:02:17 +0330

    fixed build directory error

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
-mkdir build
+if not exist build (
+    mkdir build
+)
 cd build
 cmake .. -DBUILD_SHARED_LIBS=OFF -DSDL_STATIC=ON -DSDL_SHARED=OFF
 cmake --build . --config Release
--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-mkdir build
+if [ ! -d build ]; then
+    mkdir build
+fi
+
 cd build
 cmake .. -DBUILD_SHARED_LIBS=OFF -DSDL_STATIC=ON -DSDL_SHARED=OFF
 cmake --build . --config Release
