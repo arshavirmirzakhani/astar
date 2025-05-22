ddbfb27e9e3b55d60d6bad023b9f47712d026b93
arshavir mirzakhani <arshavir2468@gmail.com>
2025-05-22 23:09:50 +0330

    adding branching

--- engine/logic_system/logic.cpp
+++ engine/logic_system/logic.cpp
@@ -6,22 +6,17 @@ LogicEngine::~LogicEngine() {}
 
 void LogicEngine::process() {
 
-	LogicBlock current_block = this->first_block;
+	LogicBlock* current_block = &this->first_block;
 
-	while (true) {
-		this->process_block(&current_block);
-
-		if (current_block.next_block == nullptr) {
-			break;
-		} else {
-			current_block = *current_block.next_block;
-		}
+	while (current_block != nullptr) {
+		this->process_block(current_block);
+		current_block = current_block->next_block; // Move to next block
 	}
 }
 
 void LogicEngine::process_block(LogicBlock* block) {
 	switch (block->type) {
-	case LOGIC_BLOCK_TYPE::NONE:
+	case LOGIC_BLOCK_TYPE::NONE_LOGIC:
 		break;
 
 	case LOGIC_BLOCK_TYPE::PRINT:
@@ -32,6 +27,26 @@ void LogicEngine::process_block(LogicBlock* block) {
 				    std::cout << "[LogicBlock Pointer]" << std::endl;
 			    } else {
 				    std::cout << arg << std::endl;
+			    }
+		    },
+		    block->arguments[0]);
+
+	case LOGIC_BLOCK_TYPE::BRANCH:
+		std::visit(
+		    [&block](auto&& arg) {
+			    using T = std::decay_t<decltype(arg)>;
+			    if constexpr (std::is_same_v<T, LogicOperator*>) {
+				    arg->process();
+
+				    using T = std::decay_t<decltype(arg->result_value)>;
+
+				    if constexpr (!std ::is_same_v<T, bool>) {
+
+					    using T = std::decay_t<decltype(block->arguments[1])>;
+					    if constexpr (std::is_same_v<T, LogicBlock*>) {
+						    block->next_block = block->arguments[1]
+					    }
+				    }
 			    }
 		    },
 		    block->arguments[0]);
--- engine/logic_system/logic.h
+++ engine/logic_system/logic.h
@@ -7,30 +7,41 @@
 
 // Define enum for logic block types
 enum LOGIC_BLOCK_TYPE {
-	NONE,
+	NONE_LOGIC,
 	PRINT,
+	BRANCH,
+	BRANCH_ELSE,
 };
 
-// Forward declaration
+enum LOGIC_OPERATOR_TYPE { NONE_OP, NUMBER, ADD, SUB, MUL, DIV, EQUAL, NEQUAL, GREATER, LESSER, GREATERE, LESSERE };
+
 struct LogicBlock;
 
-// ArgumentType supports int, float, string, and pointer to another LogicBlock
-using ArgumentType = std::variant<int, float, std::string, LogicBlock*>;
+struct LogicOperator;
 
-// Represents a single logic instruction
+// all data types
+using ArgumentType = std::variant<int, float, bool, std::string, LogicBlock*, LogicOperator*>;
+
 struct LogicBlock {
-	LOGIC_BLOCK_TYPE type = LOGIC_BLOCK_TYPE::NONE;
+	LOGIC_BLOCK_TYPE type = LOGIC_BLOCK_TYPE::NONE_LOGIC;
 	std::vector<ArgumentType> arguments;
 	LogicBlock* next_block = nullptr;
 };
 
-// Represents a function composed of logic blocks
+struct LogicOperator {
+	LOGIC_OPERATOR_TYPE type = LOGIC_OPERATOR_TYPE::NONE_OP;
+	ArgumentType left_value;
+	ArgumentType right_value;
+	ArgumentType result_value;
+
+	void process() {};
+};
+
 struct LogicFunction {
 	std::string function_name;
 	LogicBlock first_block;
 };
 
-// Engine to process logic blocks
 class LogicEngine {
       public:
 	LogicEngine();
